263ef8f5ba4b0fcfbb0747d001dc8424cd54bab0
lor104 <lauren.rosentzveig@gmail.com>
2017-05-04 10:13:06 -0400

    finish solution for ex1

--- ex1.js
+++ ex1.js
@@ -30,4 +30,34 @@ function display(color) {
   }
 };
 
-display(["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"]);
+// display(["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"]);
+
+// =====================solution========================
+
+var choices = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+var suffixes = ["th","st","nd","rd"];
+
+let result = choices.map(function(choice, index) {
+  let suffix;
+  switch(index) {
+    case 0:
+      //suffix should be st
+      suffix = suffixes[1];
+      break;
+    case 1:
+      //suffix should be nd
+      suffix = suffixes[2];
+      break;
+    case 2:
+      //suffix should be rd
+      suffix = suffixes[3];
+      break;
+    default:
+      //suffix should be th
+      suffix = suffixes[0];
+      break;
+  }
+  return `${index + 1}${suffix} choice is ${choice}`
+});
+
+console.log(result)
